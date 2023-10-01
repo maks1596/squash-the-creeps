d4a057b42e20e233b40974aa74c31a84407e02f5
Максим Шестопёров <maks1596@mail.ru>
2023-10-01 14:41:21 +0300

    Добавил инициализацию gui

--- scripts/gui.gd
+++ scripts/gui.gd
@@ -5,6 +5,7 @@ const _SCORE_FORMAT = "Score: %d"
 @onready var score_label = $ScoreLabel as Label
 
 func _ready():
+	_on_score_changed(Score.get_score())
 	Score.changed.connect(_on_score_changed)
 
 
--- scripts/score.gd
+++ scripts/score.gd
@@ -4,6 +4,8 @@ signal changed(score: int)
 
 var _score := 0
 
+func get_score() -> int: return _score
+
 func increase():
 	var new_score = _score + 1
 	_score = new_score

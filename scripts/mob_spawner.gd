cf0cefab96ba99581f1985c41bcb59e824064a29
Максим Шестопёров <maks1596@mail.ru>
2023-10-01 12:57:32 +0300

    Добавил экспорт угла отклонения моба

--- scripts/mob_spawner.gd
+++ scripts/mob_spawner.gd
@@ -1,18 +1,14 @@
 @tool
 extends Node
 
-const MOB_DIRECTION_DIVIATION = PI / 4
-
-var _player_position: Vector3:
-	get: return player.position
-
 @export var spawn_parent: Node
 @export var player: Node3D
 @export var mobs: Array[PackedScene] = []
 @export_range(0, 1, 1, "or_greater", "suffix:m/s") var mob_min_speed = 10
 @export_range(1, 1, 1, "or_greater", "suffix:m/s") var mob_max_speed = 18
+@export_range(0, 180, 1, "radians") var mob_direction_diviation := PI / 4
 
-@onready var spawn_location = $SpawnPath/SpawnLocation as PathFollow3D
+@onready var _spawn_location = $SpawnPath/SpawnLocation as PathFollow3D
 @onready var _spawn_timer = $Timer as Timer
 
 func start():
@@ -20,18 +16,18 @@ func start():
 
 
 func stop():
-	pass
 	_spawn_timer.stop()
 
 
 func _spawn_mob():
+	var target_position = player.position
 	var mob_scene = mobs.pick_random() as PackedScene
 	var mob = mob_scene.instantiate() as Mob
 	
 	var spawn_position = _generate_spawn_position()
 	var direction_diviation = _generate_direction_diviation()
 	
-	mob.look_at_from_position(spawn_position, _player_position)
+	mob.look_at_from_position(spawn_position, target_position)
 	mob.rotation.x = 0
 	mob.rotate_y(direction_diviation)
 	mob.velocity = _generate_velocity(mob.rotation)
@@ -40,12 +36,12 @@ func _spawn_mob():
 
 
 func _generate_spawn_position() -> Vector3:
-	spawn_location.progress_ratio = randf()
-	return spawn_location.position
+	_spawn_location.progress_ratio = randf()
+	return _spawn_location.position
 
 
 func _generate_direction_diviation() -> float:
-	return randf_range(-MOB_DIRECTION_DIVIATION, MOB_DIRECTION_DIVIATION)
+	return randf_range(-mob_direction_diviation, mob_direction_diviation)
 
 
 func _generate_velocity(current_rotation: Vector3) -> Vector3:
@@ -56,7 +52,7 @@ func _generate_velocity(current_rotation: Vector3) -> Vector3:
 
 func _generate_direction(mob: Mob, target_position: Vector3):
 	mob.look_at(target_position)
-	var angle = randf_range(-MOB_DIRECTION_DIVIATION, MOB_DIRECTION_DIVIATION)
+	var angle = randf_range(-mob_direction_diviation, mob_direction_diviation)
 	mob.rotate_y(angle)
 
 
